6e67087aaedff45e2c43c824c4bae9303adb33f6
Tyler Woolsey <Tyler Woolsey>
2016-04-22 13:07:32 -0500

    EAB_INFO COMMIT

--- EABTracker/app/src/main/java/com/team1/android/eabtracker/EABInfoFragment.java
+++ EABTracker/app/src/main/java/com/team1/android/eabtracker/EABInfoFragment.java
@@ -1,11 +1,25 @@
 package com.team1.android.eabtracker;
 
 
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.TextViewCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
+import android.widget.MediaController;
+import android.widget.TextView;
+import android.widget.VideoView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EABInfoFragment extends Fragment {
 
@@ -27,9 +41,17 @@ public class EABInfoFragment extends Fragment {
            deleteButton = (Button) rootView.findViewById(R.id.DeleteButton);
         */
         View rootView = inflater.inflate(R.layout.fragment_eabinfo, container, false);
+
+        /*VideoView videoView = (VideoView) rootView.findViewById(R.id.VideoView);
+        MediaController mc = new MediaController(this.getContext());
+        mc.setAnchorView(videoView);
+        Uri uri = Uri.parse("rtsp://r4---sn-vgqsen7r.googlevideo.com/Cj0LENy73wIaNAmH-KaxaIQDWBMYDSANFC1l_RdXMOCoAUIASARgqKaqoqXIyYFXigELWk0zSGpqRDZhMGMM/5A38524F36FE7ABC39DCB41CC30439F40AFDFB6B.BDEB5B85901DA6C91FBEA8655085F70D6105C76C/yt6/1/video.3gp");
+        videoView.setMediaController(mc);
+        videoView.setVideoURI(uri);*/
+        //videoView.requestFocus();
+        //videoView.start();
+
         return rootView;
 
     }
-
-
 }
--- EABTracker/app/src/main/java/com/team1/android/eabtracker/MainActivity.java
+++ EABTracker/app/src/main/java/com/team1/android/eabtracker/MainActivity.java
@@ -1,5 +1,6 @@
 package com.team1.android.eabtracker;
 
+import android.support.annotation.ColorInt;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -17,6 +18,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -44,12 +47,19 @@ public class MainActivity extends AppCompatActivity {
         tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
 
+        Window window = this.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(this.getResources().getColor(R.color.colorPrimaryDark));
+        window.setNavigationBarColor(this.getResources().getColor(R.color.colorPrimaryDark));
+
     }
 
     private void setupViewPager(ViewPager viewPager) {
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
         adapter.addFragment(new MapFragment(), "Map");
         adapter.addFragment(new EABInfoFragment(), "EAB Info");
+        adapter.addFragment(new YoutubeFragment(), "EAB Video");
         adapter.addFragment(new AshInfoFragment(), "Ash Info");
         viewPager.setAdapter(adapter);
     }
